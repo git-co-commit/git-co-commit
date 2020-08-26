6c1e755099a054309c412b3a4912d1ded7cad755
GokulnathP <4gokul2000@gmail.com>
2020-08-26 18:14:51 +0530

    Readme updated

--- README.md
+++ README.md
@@ -1,32 +1,44 @@
 # git-co-commit
 
-Git co-commit is the npm package that enables multiple users to pair and commit at the same time.
+When a pair or multiple developers work on git projects, there is a convention that allows us to document our co-authors in the commit message. However, adding the co-authorship information on every single commit is painful. <br><br>
+Install git-co-commit package to use just a single commit to add all the team members that worked in the project as co-authors. <br><br>
+Each co-author will be added to the commit message, so that they are also credited in the github contributions view!
 
-Install it globally,
+### Before pairing
+
+![before pairing](https://raw.githubusercontent.com/bdo/gitpair/master/docs/before-gitpair.png)
+
+### After pairing
+
+![after pairing](https://raw.githubusercontent.com/bdo/gitpair/master/docs/after-gitpair.png)
+
+<br>
+
+## Install it globally,
 
 ```bash
 npm i -g git-co-commit
 ```
 
-Use it in global,
+#### Use it globally,
 
 ```bash
 commit
 ```
 
-Install it locally,
+## Install it locally,
 
 ```bash
 npm i git-co-commit
 ```
 
-For use it in local specify following in scripts of package.json
+#### For using it locally, specify following in scripts of package.json
 
 ```js
 "commit": "commit"
 ```
 
-For run it
+#### For running it
 
 ```bash
 npm run commit
--- commit.js
+++ commit.js
@@ -2,7 +2,7 @@
 
 // Package to run terminal commands and Getting inputs
 const { exec } = require('child_process');
-const prompt = require('prompt-sync')({sigint: true});
+const prompt = require('prompt-sync')({ sigint: true });
 
 // Package for checking os
 let os = require('os');
@@ -12,41 +12,41 @@ let log = (err, stdout, stderr) => console.log(stdout);
 
 // Getting commit message
 let message = prompt('Your Commit message: ');
-message = (message==='') ? "Some changes maded in this commit" : message;
+message = (message === '') ? "Some changes made in this commit" : message;
 
 // Getting pairs username
 let inputPairs = prompt('Pairs username seperated by comma: ');
 let pairs = [];
 inputPairs.split(",").forEach(pair => {
-	if(pair.trim() !== ''){
+	if (pair.trim() !== '') {
 		pairs.push(pair.trim());
 	}
 });
 
 // Command to be executed
 let command = `git commit -m \"${message}\"`;
-if(pairs.length > 0){
+if (pairs.length > 0) {
 	command += ` -m \"Co-authored-by: ${pairs[0]} <${pairs[0].toLowerCase()}@users.noreply.github.com>`;
 }
 
 // Running command based on OS
-if(os.type() === 'Windows_NT'){
-	if(pairs.length > 1){
-		for(var i = 1; i < pairs.length; i++){
+if (os.type() === 'Windows_NT') {
+	if (pairs.length > 1) {
+		for (var i = 1; i < pairs.length; i++) {
 			command += `\`nCo-authored-by: ${pairs[i]} <${pairs[i].toLowerCase()}@users.noreply.github.com>`;
 		}
 	}
-	if(pairs.length > 0){
+	if (pairs.length > 0) {
 		command += "\"";
 	}
-	exec(command, {'shell': 'powershell.exe'}, log);
+	exec(command, { 'shell': 'powershell.exe' }, log);
 } else {
-	if(pairs.length > 1){
-		for(var i = 1; i < pairs.length; i++){
+	if (pairs.length > 1) {
+		for (var i = 1; i < pairs.length; i++) {
 			command += `\nCo-authored-by: ${pairs[i]} <${pairs[i].toLowerCase()}@users.noreply.github.com>`;
 		}
 	}
-	if(pairs.length > 0){
+	if (pairs.length > 0) {
 		command += "\"";
 	}
 	exec(command, log);
